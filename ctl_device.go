bc61d5ebd00d48538f3658b6018b92c4b4e48a70
0xYeah <wmyeah017@gmail.com>
2026-03-21 00:09:47 +0800

    fix actions node warning

--- ctl_device.go
+++ ctl_device.go
@@ -1,1 +1,13 @@
 package ctl_device
+
+import "github.com/0xdevelop/ctl_device/config"
+
+// Version [☑]Option
+/*
+	en: Get `ctl_device` version;
+	zh-CN: 获取`ctl_device`版本;
+	@return [☑]string en: version string;zh-CN: 版本字符串;
+*/
+func Version() string {
+	return config.ProjectVersion
+}
